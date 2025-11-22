e5699d9b3bacd5d92bcbad9ae6659368f60cac46
JimmyLin <lexusjimmy2002@gmail.com>
2025-11-22 18:06:18 +0900

    add: add functional programing related rules and package

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,43 @@
+import process from 'node:process'
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import functional from 'eslint-plugin-functional'
+import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
+        tsconfigRootDir: process.cwd(),
       },
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      functional,
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      ...functional.configs.recommended.rules,
+      'functional/no-let': 'error',
+      'functional/immutable-data': 'error',
+      'functional/functional-parameters': 'off',
+      'functional/no-expression-statements': 'off',
+      'functional/no-return-void': 'off',
     },
   },
 ])

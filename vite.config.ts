c837065135e9cdf7d9fcd439b605818392b5e3bb
JimmyLin <lexusjimmy2002@gmail.com>
2025-11-30 02:01:05 +0900

    fix: reinstall with npx

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,8 @@
-import * as path from "path";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import rollupReplace from "@rollup/plugin-replace";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    rollupReplace({
-      preventAssignment: true,
-      values: {
-        "process.env.NODE_ENV": JSON.stringify("development"),
-      },
-    }),
-    react(),
-  ],
-  resolve: process.env.USE_SOURCE
-    ? {
-        alias: {
-          "react-router": path.resolve(
-            __dirname,
-            "../../packages/react-router/index.ts",
-          ),
-          "react-router-dom": path.resolve(
-            __dirname,
-            "../../packages/react-router-dom/index.tsx",
-          ),
-        },
-      }
-    : {},
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
